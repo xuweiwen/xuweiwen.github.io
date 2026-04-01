6380feecbedca1b2abe97c19e6f37a096ec9fc58
Xu, Wei <xw1224@gmail.com>
2026-04-01 19:19:45 -0400

    Update

--- assets/js/utils/move-focus-skip-link.js
+++ assets/js/utils/move-focus-skip-link.js
@@ -9,13 +9,10 @@ function initMoveFocusSkipLink(linkId) {
   const link = document.getElementById(linkId);
   if (!link) return;
   link.addEventListener('click', (event) => {
-    event.preventDefault();
     const targetId = link.getAttribute('href')?.substring(1);
     if (!targetId) return;
     const targetElement = document.getElementById(targetId);
     if (!targetElement) return;
-    
-    // Scroll and focus on the target element
     targetElement.scrollIntoView({ behavior: 'smooth' });
     targetElement.setAttribute('tabindex', '-1');
     targetElement.focus();
