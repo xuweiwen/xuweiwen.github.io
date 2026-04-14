239d2b87d11e587c971b540231d882fb3011557e
Xu, Wei <xw1224@gmail.com>
2026-04-14 12:08:28 -0400

    Update

--- assets/js/modules/index.js
+++ assets/js/modules/index.js
@@ -7,3 +7,4 @@ export { initModals } from './modal.js';
 export { initExternalLinks } from './external-link.js';
 export { initVideoAspectRatio } from './video-aspect-ratio.js';
 export { initBackToTop } from './back-to-up.js';
+export { initAboutMeAnchor } from './about-me-anchor.js';
